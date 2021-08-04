b7a61e587ea0998a36c12fd92b42d7c92c5c8ca8
Ashish Kulkarni <80530827+Ashishkulkarni00@users.noreply.github.com>
2021-08-05 01:39:17 +0530

    Update EmptyQueueException.java

--- EmptyQueueException.java
+++ EmptyQueueException.java
@@ -1,5 +1,6 @@
 package projects;
-
+// Exception class just to throw an exception...
+// if Queue is already Empty and somebody tries to Dequeue from the Queue then EmptyQueueException will be thrown...
 public class EmptyQueueException extends Exception {
 
 	/**
